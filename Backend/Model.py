7bbd19e4b745ae0a08ff89cfb49b58c8fb81ac6e
Rajashree Parameswaran <raji@Rajashrees-MacBook-Air.local>
2020-06-08 16:35:58 +0530

    Classified Backend Files

---
+++ Backend/Model.py
@@ -0,0 +1,29 @@
+from sklearn.model_selection import  train_test_split
+from keras.layers import Dense, Conv2D, Dropout, Flatten, MaxPooling2D
+import tensorflow as tf
+import numpy as np
+from keras.models import Sequential
+from keras.datasets import mnist
+
+input_shape = (28, 28, 1)
+
+def buildModel(weight):
+    model = Sequential()
+    model.add(Conv2D(32, kernel_size=(3, 3),activation='relu',input_shape=input_shape))
+    model.add(Conv2D(64, (3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.2))
+    model.add(Conv2D(32, (5, 5), activation='relu'))
+    model.add(Conv2D(64, (5, 5), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+    model.add(Flatten())
+    model.add(Dense(256, activation='relu'))
+    model.add(Dropout(0.2))
+    model.add(Dense(128, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(11, activation='softmax'))
+
+    model.load_weights(weight)
+
+    return model
